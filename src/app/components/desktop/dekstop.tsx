75188d5ddb717abd7793c13c5d9f4c9ae232e802
Mikael-Leger <mikael.leger8@gmail.com>
2025-01-04 19:55:47 +0100

    feature/responsive-desktop

--- src/app/components/desktop/dekstop.tsx
+++ src/app/components/desktop/dekstop.tsx
@@ -1,8 +1,10 @@
 import { RefObject, useEffect } from "react";
+
 import Window, { WindowProps } from "../window/window";
 import WindowRef from "@/app/interfaces/window-ref.interface";
 import TabInterface from "@/app/interfaces/tab.interface";
 import Shortcut from "../shortcut/shortcut";
+import { useIsMobile } from "@/app/contexts/mobile-context";
 
 import "./dekstop.scss";
 
@@ -15,35 +17,37 @@ type DekstopProps = {
 };
 
 export default function Dekstop({ windows, setWindowRef, windowRefs, getDefaultTabs, desktopOpenActions }: DekstopProps) {
+    const { isMobile } = useIsMobile();
+
     const shortcuts = [
         {
             title: "Welcome Page",
             iconPath: "/browsers/edge.png",
-            position: { top: 200, left: 200 },
+            position: { top: isMobile ? 50 : 200, left: isMobile ? 50 : 200 },
             onClick: () => desktopOpenActions("openWindow", { id: 0, name: "Welcome - Mikaël Léger" })
         },
         {
             title: "Portfolio",
-            position: { top: 200, left: 350 },
+            position: { top: isMobile ? 175 : 200, left: isMobile ? 50 : 350 },
             iconPath: "/browsers/edge.png",
             onClick: () => desktopOpenActions("openWindow", { id: 0, name: "Portfolio" })
         },
         {
             title: "Skills",
             iconPath: "/browsers/edge.png",
-            position: { top: 350, left: 200 },
+            position: { top: isMobile ? 300 : 350, left: isMobile ? 50 : 200 },
             onClick: () => desktopOpenActions("openWindow", { id: 0, name: "Skills" })
         },
         {
             title: "CV",
             iconPath: "/icons/pdf.png",
-            position: { top: 100, right: 200 },
+            position: { top: isMobile ? 50 : 100, right: isMobile ? 50 : 200 },
             onClick: () => desktopOpenActions("openWindow", { id: 2 })
         },
         {
             title: "Contact me",
             iconPath: "/icons/mail.png",
-            position: { top: 500, right: 300 },
+            position: { top: isMobile ? 300 : 500, right: isMobile ? 50 : 300 },
             onClick: () => desktopOpenActions("openWindow", { id: 3 })
         }
     ];
--- src/app/components/task-bar/task-bar.tsx
+++ src/app/components/task-bar/task-bar.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useIsMobile } from "@/app/contexts/mobile-context";
 
 import "./task-bar.scss";
 
@@ -6,6 +7,8 @@ type TaskBarProps = {
 };
 
 export default function TaskBar({ }: TaskBarProps) {
+    const { isMobile } = useIsMobile();
+
     const restartWebsiteAnimations = () => {
         localStorage.removeItem('first-animation');
         localStorage.removeItem('active-tab');
@@ -14,12 +17,14 @@ export default function TaskBar({ }: TaskBarProps) {
     }
 
     return (
-        <div className="task-bar">
+        <div className="task-bar" style={{ padding: isMobile ? "0 20px" : "0 40px" }}>
             <div className="task-bar-restart" onClick={restartWebsiteAnimations}>
                 <img className="task-bar-restart-img" src="/icons/restart.png" />
-                <div className="task-bar-restart-button">
-                    Restart
-                </div>
+                {!isMobile && (
+                    <div className="task-bar-restart-button">
+                        Restart
+                    </div>
+                )}
             </div>
             <div className="task-bar-tabs">
             </div>
